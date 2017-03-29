38164c9dd1f0d28e873cf2ae418ec21c4d73f126
Michael 'Maik' Ardan <michael_ardan2000@yahoo.com>
2017-03-29 22:49:21 +0800

    [Re-commit] Added Filters

--- mf-scheduler.js
+++ mf-scheduler.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var taskTypeModule = require("./lib/task-type.js");
+var filters = require("./lib/filters.js");
 var app = express();
 var port = 3000;
 
@@ -6,6 +8,18 @@ app.engine(".html", require("ejs").__express);
 app.set("views", __dirname + "/views");
 app.set("view engine", "html");
 app.use(express.static(__dirname));
+
+app.get("/get-task-type", function(request, response) {
+    taskTypeModule.listTaskType(request, response);
+});
+
+app.get("/get-hours", function(request, response) {
+    filters.listHours(request, response);
+});
+
+app.get("/get-intervals", function(request, response) {
+    filters.listIntervals(request, response);     
+});
 
 app.get("*", function(request, response) {
     response.render("index");
